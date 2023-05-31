111cf5d88dc6991eb91056fd1dc8edd168af1c2d
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 16:42:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.title("Hello World")
+st.title("SWAMP Blimps")
 st.balloons()
 
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        </style>
+                        """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
