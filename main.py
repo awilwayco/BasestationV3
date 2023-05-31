bb60fe27361dc85c018a0ffe151111cd6e7f456f
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 16:40:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import streamlit as st
 st.title("Hello World")
 st.balloons()
 
-def local_css(file_name):
-  with open(file_name) as f:
-    st.markdown(f"<style{f.read()}</style>", unsafe_allow_html=True)
-    
-local_css("style/style.css")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
