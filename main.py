d97ac4fa959d36e4c06428637bb6becc3078a608
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 21:02:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # Title
 st.markdown("<h1 style='text-align: center; color: white;'>SWAMP Blimps</h1>", unsafe_allow_html=True)
 
-if 'button_state' not in st.session_state:
-    st.session_state['button_state'] = False
+@st.cache(allow_output_mutation=True)
+def read_button_state():
+    with open("database.txt", "r") as file:
+        return file.read() == "True"
 
-if st.button("True/False Button"):
-    st.session_state['button_state'] = not st.session_state['button_state']
+@st.cache(allow_output_mutation=True)
+def write_button_state(button_state):
+    with open("database.txt", "w") as file:
+        file.write(str(button_state))
 
-if st.session_state['button_state']:
+button_state = read_button_state()
+
+if st.button("True/False Button", key="button"):
+    button_state = not button_state
+    write_button_state(button_state)
+
+if button_state:
     st.write("Button is True")
 else:
     st.write("Button is False")
