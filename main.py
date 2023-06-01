1939a16678b166a96f24daa462d6381de0c08b50
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 20:44:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,17 +12,22 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # Title
 st.markdown("<h1 style='text-align: center; color: white;'>SWAMP Blimps</h1>", unsafe_allow_html=True)
 
-# Read the current button state from the file
-with open("database.txt", "r") as file:
-    button_state = file.read() == "True"
+@st.cache(allow_output_mutation=True)
+def read_button_state():
+    with open("database.txt", "r") as file:
+        return file.read() == "True"
 
-# Create a button and update the file with the new state when clicked
-if st.button("True/False Button", key="button"):
-    button_state = not button_state
+@st.cache(allow_output_mutation=True)
+def write_button_state(button_state):
     with open("database.txt", "w") as file:
         file.write(str(button_state))
 
-# Display the boolean value based on the button state
+button_state = read_button_state()
+
+if st.button("True/False Button", key="button"):
+    button_state = not button_state
+    write_button_state(button_state)
+
 if button_state:
     st.write("Button is True")
 else:
