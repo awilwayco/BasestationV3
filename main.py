884aec2c85669728fa9a53ca9db2d16d2813b266
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 20:58:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; color: white;'>SWAMP Blimps</h1>", unsafe_allow_html=True)
 
 if 'button_state' not in st.session_state:
-    st.session_state.button_state = False
+    st.session_state['button_state'] = False
 
 if st.button("True/False Button"):
-    st.session_state.button_state = not st.session_state.button_state
+    st.session_state['button_state'] = not st.session_state['button_state']
 
-if st.session_state.button_state:
+if st.session_state['button_state']:
     st.write("Button is True")
 else:
     st.write("Button is False")
