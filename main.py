44fb41152e85c85eb8555bdfa2b3d7be96c5cf76
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 20:24:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+title_alignment=
+"""
+<style>
+#the-title {
+  text-align: center
+}
+</style>
+"""
+st.markdown(title_alignment, unsafe_allow_html=True)
+
 st.title("SWAMP Blimps")
 
 hide_streamlit_style = """
