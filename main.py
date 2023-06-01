024a3dc483bdc564dea8eeda4705c5c283ad5841
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 20:31:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Title
 st.markdown("<h1 style='text-align: center; color: white;'>SWAMP Blimps</h1>", unsafe_allow_html=True)
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
