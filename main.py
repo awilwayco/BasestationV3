aa32c28a7b1005ce26e198e67692a9ab443adde1
awilwayco <56363833+awilwayco@users.noreply.github.com>
2023-05-31 20:30:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
-
-st.title("SWAMP Blimps")
-
+# Hide Streamlit Style
 hide_streamlit_style = """
                         <style>
                         #MainMenu {visibility: hidden;}
@@ -11,3 +8,6 @@ hide_streamlit_style = """
                         </style>
                         """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+# Title
+st.markdown("<h1 style='text-align: center; color: white;'>SWAMP Blimps</h1>", unsafe_allow_html=True)
